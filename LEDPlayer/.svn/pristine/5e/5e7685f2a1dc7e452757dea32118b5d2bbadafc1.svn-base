2e70741899a04681dfe8f1afb07ecae0480be627
heibai <binsxie@gmail.com>
2014-12-09 00:28:45 +0800

    del player

--- LEDPlayer/.svn/pristine/5e/5e7685f2a1dc7e452757dea32118b5d2bbadafc1.svn-base
+++
@@ -1,7 +0,0 @@
-package com.led.player.defview.gif1;
-
-public interface PrepareListener {
-	
-	public void onPrepare(int count);
-	public void onDecodeEnd(int status);
-}
--- LEDPlayer/src/com/led/player/defview/gif3/PrepareListener.java
+++
@@ -1,9 +0,0 @@
-package com.led.player.defview.gif3;
-
-import android.graphics.Bitmap;
-
-public interface PrepareListener {
-	
-	public void onPrepare(int count, Bitmap image, int delay);
-	public void onDecodeEnd(int status);
-}
